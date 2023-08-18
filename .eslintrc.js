6030b308117168c6bf84a079fb59456867af45a8
Kazuya Yokoyama <yk3kzy@gmail.com>
2023-08-19 07:55:37 +0900

    lint が通るように修正

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,18 @@
 /** @type {import('eslint').ESLint.ConfigData} */
 module.exports = {
   extends: [
-    "plugin:@typescript-eslint/recommended",
-    "next/core-web-vitals",
-    "prettier"
-  ]
-};
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:@typescript-eslint/recommended',
+    'next/core-web-vitals',
+    'prettier',
+  ],
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'off',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+}
